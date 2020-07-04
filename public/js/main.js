e4574962f72a6c179f3f782b067faf435d7d59be
ywwwtseng <ywwwtseng@gmail.com>
2020-07-04 20:26:39 +0800

    feat: create mario entity

--- public/js/main.js
+++ public/js/main.js
@@ -1,36 +1,34 @@
 import Compositor from './Compositor.js';
 import {loadLevel} from './loaders.js';
-import {loadMarioSprite, loadBackgroundSprites} from './sprites.js';
-import {createBackgroundLayer} from './layers.js';
+import {createMario} from './entities.js';
+import {loadBackgroundSprites} from './sprites.js';
+import {createBackgroundLayer, createSpriteLayer} from './layers.js';
+
 
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
-function createSpriteLayer(sprite, pos) {
-  return function drawSpriteLayer(context) {
-    sprite.draw('idle', context, pos.x, pos.y);
-  };
-}
-
 Promise.all([
-  loadMarioSprite(),
+  createMario(),
   loadBackgroundSprites(),
   loadLevel('1-1'),
 ])
-.then(([marioSprite, backgroundSprites, level]) => {
+.then(([mario, backgroundSprites, level]) => {
   const comp = new Compositor();
   const backgroundLayer = createBackgroundLayer(level.backgrounds, backgroundSprites);
   comp.layers.push(backgroundLayer);
 
-  const pos = {x: 64, y: 64};
+  const gravity = 0.5;
 
-  const spriteLayer = createSpriteLayer(marioSprite, pos);
+  
+
+  const spriteLayer = createSpriteLayer(mario);
   comp.layers.push(spriteLayer);
 
   function update() {
     comp.draw(context);
-    pos.x +=2;
-    pos.y +=2;
+    mario.update();
+    mario.vel.y += gravity;
     requestAnimationFrame(update);
   }
 
