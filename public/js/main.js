12c84b06b3597021c2f721d24458f81badf8d8ce
ywwwtseng <ywwwtseng@gmail.com>
2020-07-12 11:26:10 +0800

    feat: Implement Level class

--- public/js/main.js
+++ public/js/main.js
@@ -2,8 +2,7 @@ import Compositor from './Compositor.js';
 import Timer from './Timer.js';
 import {loadLevel} from './loaders.js';
 import {createMario} from './entities.js';
-import {loadBackgroundSprites} from './sprites.js';
-import {createBackgroundLayer, createSpriteLayer} from './layers.js';
+
 import Keyboard from './KeyboardState.js';
 
 
@@ -14,17 +13,13 @@ const context = canvas.getContext('2d');
 
 Promise.all([
   createMario(),
-  loadBackgroundSprites(),
   loadLevel('1-1'),
 ])
-.then(([mario, backgroundSprites, level]) => {
-  const comp = new Compositor();
-
-  const backgroundLayer = createBackgroundLayer(level.backgrounds, backgroundSprites);
-  comp.layers.push(backgroundLayer);
-
+.then(([mario, level]) => {
   const gravity = 2000;
   mario.pos.set(64, 180);
+
+  level.entities.add(mario);
   
   const SPACE = 32;
   const input = new Keyboard();
@@ -37,13 +32,10 @@ Promise.all([
   });
   input.listenTo(window);
 
-  const spriteLayer = createSpriteLayer(mario);
-  comp.layers.push(spriteLayer);
-
   const timer = new Timer(1/60);
   timer.update = function update(deltaTime) {
-    comp.draw(context);
-    mario.update(deltaTime);
+    level.update(deltaTime);
+    level.comp.draw(context);
     mario.vel.y += gravity * deltaTime;
   }
   timer.start();
