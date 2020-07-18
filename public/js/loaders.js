686e00c6658c4db57db89aa49f385db627c42caf
ywwwtseng <ywwwtseng@gmail.com>
2020-07-18 12:47:46 +0800

    feat: implement Matrix and use it to create level.tiles

--- public/js/loaders.js
+++ public/js/loaders.js
@@ -12,6 +12,20 @@ export function loadImage(url) {
   });
 }
 
+export function createTiles(level, backgrounds) {
+  backgrounds.forEach(background => {
+    background.ranges.forEach(([x1, x2, y1, y2]) => {
+      for (let x = x1; x < x2; ++x) {
+        for (let y = y1; y < y2; ++y) {
+          level.tiles.set(x, y, {
+            name: background.tile,
+          });
+        }
+      }
+    });
+  });
+}
+
 export function loadLevel(name) {
   return Promise.all([
     fetch(`/levels/${name}.json`).then(r => r.json()),
@@ -20,7 +34,9 @@ export function loadLevel(name) {
     .then(([levelSpec, backgroundSprites]) => {
       const level = new Level();
 
-      const backgroundLayer = createBackgroundLayer(levelSpec.backgrounds, backgroundSprites);
+      createTiles(level, levelSpec.backgrounds);
+
+      const backgroundLayer = createBackgroundLayer(level, backgroundSprites);
       level.comp.layers.push(backgroundLayer);
 
       const spriteLayer = createSpriteLayer(level.entities);
